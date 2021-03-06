6f329373647e1c18c1e082b610e2951b309d1fc2
maxhannan <maxhannan@substanceseven.com>
2021-03-06 15:21:04 -0600

    fixes

--- src/Components/Forecast_Components/MainForecast.js
+++ src/Components/Forecast_Components/MainForecast.js
@@ -1,18 +1,15 @@
 const MainForecast = ({data}) => {
   return ( 
-    <div className=" MainForecast">
-      <div className="forecastItem location">
-        <h1 style = {{fontSize: '32pt', fontWeight: '500'}} id = 'loco'>{data.location.split(',')[0]}</h1>
-        <h2 id = 'descrip'>{data.descrip}</h2>
-      </div>
-      <div className='forecastItem MainTemp'>
-      <h1 style = {{fontSize: '90pt', fontWeight: '500'}}>{data.tempF}°</h1>
-      </div> 
-      <div className="forecastItem mainImgContainer">
-        <h1 style = {{fontSize: '32pt', fontWeight: '500'}} id = 'loco'>{data.location.split(',')[0]}</h1>
+    <div className="forecastItem MainForecast">
+      <div className="mainImgContainer">
         <img src={data.iconSrc} alt="weather icon"/>
-        <h2 id = 'descrip'>{data.descrip}</h2>
       </div> 
+      <div className='  MainTemp'>
+        <h1 style ={{fontWeight: '500'}}>{data.location.split(',')[0]}</h1>
+        <h2>{data.descrip}</h2>
+        <h1 style ={{fontWeight: '500', fontSize: '8vw'}}>{data.tempF}°</h1>
+      </div> 
+     
     </div>
    );
 }
