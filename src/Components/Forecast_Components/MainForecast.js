8f8e5d2e61c80dfecd6ecf0fd9f870a00e7c71ca
maxhannan <maxhannan@substanceseven.com>
2021-03-06 15:32:32 -0600

    fixes

--- src/Components/Forecast_Components/MainForecast.js
+++ src/Components/Forecast_Components/MainForecast.js
@@ -1,10 +1,10 @@
 const MainForecast = ({data}) => {
   return ( 
-    <div className="forecastItem MainForecast">
-      <div className="mainImgContainer">
+    <div className=" MainForecast">
+      <div className="forecastItem mainImgContainer">
         <img src={data.iconSrc} alt="weather icon"/>
       </div> 
-      <div className='  MainTemp'>
+      <div className='forecastItem MainTemp'>
         <h1 style ={{fontWeight: '500'}}>{data.location.split(',')[0]}</h1>
         <h2>{data.descrip}</h2>
         <h1 style ={{fontWeight: '500', fontSize: '8vw'}}>{data.tempF}°</h1>
